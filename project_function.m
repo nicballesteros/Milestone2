13acb6873f8ac87a7b1e133ba29ff349c3820d06
belle470 <62964258+belle470@users.noreply.github.com>
2020-03-31 17:14:47 -0400

    More Organization

--- project_function.m
+++ project_function.m
@@ -28,30 +28,52 @@ did you complete the assignment information? delete this line if yes
 %% INITIALIZATION
 
 %Importing the data
-Substrate_data = readmatrix('Data_nextGen_KEtesting_allresults.csv');
-time = Substrate_data(:,1);
+Substrate_data = readmatrix('Data_nextGen_KEtesting_allresults.csv'); %All five of the enzymes substrate data with two trials
+time = Substrate_data(:,1); %Times for each of the substrates transformations
 
 %Enzyme A orgnaization
-initial_375A = Substrate_data(:,2);
-initial_75A = Substrate_data(:,3);
-initial_15A = Substrate_data(:,4);
-initial_30A = Substrate_data(:,5);
-initial_65A = Substrate_data(:,6);
-initial_125A = Substrate_data(:,7);
-initial_250A = Substrate_data(:,8);
-initial_500A = Substrate_data(:,9);
-initial_1000A = Substrate_data(:,10);
-initial_2000A = Substrate_data(:,11);
-dup_375A = Substrate_data(:,12);
-dup_75A = Substrate_data(:,13);
-dup_15A = Substrate_data(:,14);
-dup_30A = Substrate_data(:,15);
-dup_65A = Substrate_data(:,16);
-dup_125A = Substrate_data(:,17);
-dup_250A = Substrate_data(:,18);
-dup_500A = Substrate_data(:,19);
-dup_1000A = Substrate_data(:,20);
-dup_2000A = Substrate_data(:,21);
+initial_375A = Substrate_data(:,2); % Initial Test of Enzyme A with initial concentration of 3.75 (uM)
+initial_75A = Substrate_data(:,3); % Initial Test of Enzyme A with initial concentration of 7.5 (uM)
+initial_15A = Substrate_data(:,4); % Initial Test of Enzyme A with initial concentration of 15 (uM)
+initial_30A = Substrate_data(:,5); % Initial Test of Enzyme A with initial concentration of 30 (uM)
+initial_65A = Substrate_data(:,6); % Initial Test of Enzyme A with initial concentration of 65 (uM)
+initial_125A = Substrate_data(:,7); % Initial Test of Enzyme A with initial concentration of 125 (uM)
+initial_250A = Substrate_data(:,8); % Initial Test of Enzyme A with initial concentration of 250 (uM)
+initial_500A = Substrate_data(:,9); % Initial Test of Enzyme A with initial concentration of 500 (uM)
+initial_1000A = Substrate_data(:,10); % Initial Test of Enzyme A with initial concentration of 1000 (uM)
+initial_2000A = Substrate_data(:,11); % Initial Test of Enzyme A with initial concentration of 2000 (uM)
+dup_375A = Substrate_data(:,12); % Duplicate Test of Enzyme A with initial concentration of 3.75 (uM)
+dup_75A = Substrate_data(:,13); % Duplicate Test of Enzyme A with initial concentration of 7.5 (uM)
+dup_15A = Substrate_data(:,14); % Duplicate Test of Enzyme A with initial concentration of 15 (uM)
+dup_30A = Substrate_data(:,15); % Duplicate Test of Enzyme A with initial concentration of 30 (uM)
+dup_65A = Substrate_data(:,16); % Duplicate Test of Enzyme A with initial concentration of 60 (uM)
+dup_125A = Substrate_data(:,17); % Duplicate Test of Enzyme A with initial concentration of 125 (uM)
+dup_250A = Substrate_data(:,18); % Duplicate Test of Enzyme A with initial concentration of 250 (uM)
+dup_500A = Substrate_data(:,19); % Duplicate Test of Enzyme A with initial concentration of 500 (uM)
+dup_1000A = Substrate_data(:,20); % Duplicate Test of Enzyme A with initial concentration of 1000 (uM)
+dup_2000A = Substrate_data(:,21); % Duplicate Test of Enzyme A with initial concentration of 2000 (uM)
+
+%Enzyme B orgnaization
+initial_375B = Substrate_data(:,22); % Initial Test of Enzyme B with initial concentration of 3.75 (uM)
+initial_75B = Substrate_data(:,23); % Initial Test of Enzyme B with initial concentration of 7.5 (uM)
+initial_15B = Substrate_data(:,24); % Initial Test of Enzyme B with initial concentration of 15 (uM)
+initial_30B = Substrate_data(:,25); % Initial Test of Enzyme B with initial concentration of 30 (uM)
+initial_65B = Substrate_data(:,26); % Initial Test of Enzyme B with initial concentration of 65 (uM)
+initial_125B = Substrate_data(:,27); % Initial Test of Enzyme B with initial concentration of 125 (uM)
+initial_250B = Substrate_data(:,28); % Initial Test of Enzyme B with initial concentration of 250 (uM)
+initial_500B = Substrate_data(:,29); % Initial Test of Enzyme B with initial concentration of 500 (uM)
+initial_1000B = Substrate_data(:,30); % Initial Test of Enzyme B with initial concentration of 1000 (uM)
+initial_2000B = Substrate_data(:,31); % Initial Test of Enzyme B with initial concentration of 2000 (uM)
+dup_375B = Substrate_data(:,32); % Duplicate Test of Enzyme B with initial concentration of 3.75 (uM)
+dup_75B = Substrate_data(:,33); % Duplicate Test of Enzyme B with initial concentration of 7.5 (uM)
+dup_15B = Substrate_data(:,34); % Duplicate Test of Enzyme B with initial concentration of 15 (uM)
+dup_30B = Substrate_data(:,35); % Duplicate Test of Enzyme B with initial concentration of 30 (uM)
+dup_65B = Substrate_data(:,36); % Duplicate Test of Enzyme B with initial concentration of 60 (uM)
+dup_125B = Substrate_data(:,37); % Duplicate Test of Enzyme B with initial concentration of 125 (uM)
+dup_250B = Substrate_data(:,38); % Duplicate Test of Enzyme B with initial concentration of 250 (uM)
+dup_500B = Substrate_data(:,39); % Duplicate Test of Enzyme B with initial concentration of 500 (uM)
+dup_1000B = Substrate_data(:,40); % Duplicate Test of Enzyme B with initial concentration of 1000 (uM)
+dup_2000B = Substrate_data(:,41); % Duplicate Test of Enzyme B with initial concentration of 2000 (uM)
 
 %% ____________________
 %% CALCULATIONS

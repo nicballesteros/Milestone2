615c0794d11ab9c8b0c9f0d681d8d383fff8e77e
nicballesteros <nicballeesteros2@gmail.com>
2020-04-05 13:36:39 -0400

    Account for the data being product data and not substrate data

--- main_function.m
+++ main_function.m
@@ -1,11 +1,12 @@
 % %Importing the data
-Substrate_data = readmatrix('Data_nextGen_KEtesting_allresults.csv'); %All five of the enzymes substrate data with two trials
-Substrate_data(2,:) = [];
-Substrate_data(1,:) = []; % gets rid of the headers
+product_data = readmatrix('Data_nextGen_KEtesting_allresults.csv'); %All five of the enzymes substrate data with two trials
+product_data(2,:) = [];
+product_data(1,:) = []; % gets rid of the headers
 counter = 1;
 
-time = Substrate_data(:,1); %Times for each of the substrates transformations\
-initial_375A = Substrate_data(:,2); % Initial Test of Enzyme A with initial concentration of 3.75 (uM)
+time = product_data(:,1); %Times for each of the substrates transformations\
+substate_data = 1 ./ product_data(:, 2:end);
+initial_375A = substrate_data(:,1); % Initial Test of Enzyme A with initial concentration of 3.75 (uM)
 
 
 initial_375A = rmmissing(initial_375A); %get rid of NaN values

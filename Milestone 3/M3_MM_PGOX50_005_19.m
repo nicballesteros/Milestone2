da0ef263d0d86ec1ad6d24b5366f70330b75314c
nicballesteros <nicballeesteros2@gmail.com>
2020-04-09 16:39:31 -0400

    quick fix

--- Milestone 3/M3_MM_PGOX50_005_19.m
+++ Milestone 3/M3_MM_PGOX50_005_19.m
@@ -46,9 +46,3 @@ did you complete the assignment information? delete this line if yes
 % source, either modified or unmodified. Neither have we provided
 % access to my code to another. The program we are submitting
 % is our own original work.
-<<<<<<< HEAD
-=======
-
-
-
->>>>>>> 0ccc533171161c716d79ec8c8a8fd8ca3aa9b0bc
